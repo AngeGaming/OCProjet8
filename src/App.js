fad813611204d737ed35a69f13a4ba5ca68674f8
Barbé Maël <mael.barbe47@gmail.com>
2024-02-24 20:04:31 +0100

    img and app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import './App.css';
-
+import React from "react"
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import { Header } from "./components/Header/Header";
+import { Footer } from "./components/Footer/Footer.jsx";
+import Home from "./pages/home/Home.jsx";
+import Location from "./pages/location/Location.jsx";
+import About from "./pages/about/About.jsx";
+import Error from "./pages/Error404/Error404.jsx";
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Header/>
+      <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/location/:id" element={<Location />} />
+          <Route path="/about" element={<About/>}/>
+          <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer/>
+    </Router>
   );
 }
 
